ed6447a393751413c24339ef69e78c5b37a663df
jamesbiswell <43172281+jamesbiswell@users.noreply.github.com>
2026-09-01 11:48:16 +0100

    Finishing section-08-selenium4-advanced-features

---
+++ 08-selenium4-advanced-features/src/test/java/org/luv2code/O8_selenium4_advanced_features/DevToolsTest.java
@@ -0,0 +1,42 @@
+package org.luv2code.O8_selenium4_advanced_features;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v150.network.Network;
+import org.testng.annotations.Test;
+
+import java.util.Optional;
+
+public class DevToolsTest {
+    @Test
+    public void devToolsTest() {
+
+        // set up our chrome driver
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+
+        // start devtools session
+        DevTools devTools = driver.getDevTools();
+        devTools.createSession();
+
+        // monitor network requests
+        devTools.send(Network.enable(
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty()));
+        devTools.addListener(Network.requestWillBeSent(), request ->
+                System.out.println("Request URL: " + request.getRequest().getUrl()));
+
+        System.out.println("Home Page");
+        driver.get("https://luv2test.com/#/");
+        System.out.println();
+        System.out.println("Broken images page");
+        driver.navigate().to("https://luv2test.com/#/broken-images");
+
+        driver.quit();
+
+    }
+}
