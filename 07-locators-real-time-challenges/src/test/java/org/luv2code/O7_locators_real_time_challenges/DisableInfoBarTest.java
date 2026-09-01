58c6cae0d3e601229a90e2386ea5b9f8e26202b1
jamesbiswell <43172281+jamesbiswell@users.noreply.github.com>
2026-09-01 10:01:13 +0100

    Finishing section-07-locators-real-time-challenges

---
+++ 07-locators-real-time-challenges/src/test/java/org/luv2code/O7_locators_real_time_challenges/DisableInfoBarTest.java
@@ -0,0 +1,26 @@
+package org.luv2code.O7_locators_real_time_challenges;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class DisableInfoBarTest {
+    @Test
+    public void disabledInfoBarTest() {
+
+        // set chrome options
+        ChromeOptions options = new ChromeOptions();
+
+        // disable the "Chrome is being controlled by automated test software" info bar
+        options.setExperimentalOption("excludeSwitches", new String [] {"enable-automation"});
+
+        // initialize webdriver with these options
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+
+        driver.get("https://luv2test.com/#/");
+        driver.quit();
+
+    }
+}
